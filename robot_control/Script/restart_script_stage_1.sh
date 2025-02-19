0cd21576a4d7d7f5153fbcb99577acc33492410c
M11212044 <M11212044@gemail.yuntech.edu.tw>
2025-02-19 14:36:13 +0800

    修改重新啟動腳本檔的名稱、內容以及增加Visual Serveoing要執行的程式

--- robot_control/Script/restart_script_stage_1.sh
+++ robot_control/Script/restart_script_stage_1.sh
@@ -4,7 +4,10 @@ gnome-terminal -t "Meteor Controller" --tab -- bash -c 'roslaunch four_wheel_car
 sleep 2
 
 gnome-terminal -t "ZED2 Stereo Camera" --tab -- bash -c 'roslaunch ~/Launch/ZED2_VisualSLAM/ZED2.launch'
-sleep 3
+sleep 2
+
+gnome-terminal -t "D435 Camera" --tab -- bash -c 'roslaunch apriltag_ros D435Camera_4WDD.launch'
+sleep 2
 
 gnome-terminal -t "RTAB-Map" --tab -- bash -c 'roslaunch ~/Launch/ZED2_VisualSLAM/CeilingSLAMwithZED2.launch localization:=true'
 sleep 1
@@ -25,17 +28,19 @@ while true; do
     fi
 done
 
-gnome-terminal -t "move_base" --tab -- bash -c 'roslaunch car_controller Meteor_navigation_3DLiDAR.launch'
-sleep 1
+~/project/gui_ws/src/robot_control/Script/restart_script2.sh
 
-gnome-terminal -t "Topology map" --tab -- bash -c 'roslaunch ~/Launch/TopologyMap.launch'
-sleep 1
+# gnome-terminal -t "move_base" --tab -- bash -c 'roslaunch car_controller Meteor_navigation_3DLiDAR.launch'
+# sleep 1
 
-gnome-terminal -t "laser scan" --tab -- bash -c 'roslaunch laser_scan_obstacle_detection realsense_laser_scan_obstacle_detection.launch'
-sleep 1
+# gnome-terminal -t "Topology map" --tab -- bash -c 'roslaunch ~/Launch/TopologyMap.launch'
+# sleep 1
+
+# gnome-terminal -t "laser scan" --tab -- bash -c 'roslaunch laser_scan_obstacle_detection realsense_laser_scan_obstacle_detection.launch'
+# sleep 1
 
 # python3 ~/project/gui_ws/src/robot_control/Script/test.py
-~/project/gui_ws/src/robot_control/Script/restart_script2.sh
+# ~/project/gui_ws/src/robot_control/Script/restart_script2.sh
 # roslaunch robot_control gui.launch
 # gnome-terminal -t "GUI_PY" --tab -- bash -c '~/project/gui_ws/src/robot_control/Script/test.py'
 # sleep 1
--- robot_control/Script/restart_script_stage_2.sh
+++ robot_control/Script/restart_script_stage_2.sh
@@ -25,15 +25,21 @@
 #     fi
 # done
 
-# gnome-terminal -t "move_base" --tab -- bash -c 'roslaunch car_controller Meteor_navigation_3DLiDAR.launch'
-# sleep 1
+gnome-terminal -t "move_base" --tab -- bash -c 'roslaunch car_controller Meteor_navigation_3DLiDAR.launch'
+sleep 2
 
-# gnome-terminal -t "Topology map" --tab -- bash -c 'roslaunch ~/Launch/TopologyMap.launch'
-# sleep 1
+gnome-terminal -t "Topology map" --tab -- bash -c 'roslaunch ~/Launch/TopologyMap.launch'
+sleep 1
 
-# gnome-terminal -t "laser scan" --tab -- bash -c 'roslaunch laser_scan_obstacle_detection realsense_laser_scan_obstacle_detection.launch'
-sleep 3
+gnome-terminal -t "laser scan" --tab -- bash -c 'roslaunch laser_scan_obstacle_detection realsense_laser_scan_obstacle_detection.launch'
+sleep 1
+
+gnome-terminal -t "apriltag detection" --tab -- bash -c 'roslaunch apriltag_ros continuous_detection_4WDD.launch'
+sleep 1
+
+gnome-terminal -t "PBVS" --tab -- bash -c 'roslaunch forklift_server PBVS_server_4WDD.launch'
+sleep 1
 
 roslaunch robot_control gui.launch
 # gnome-terminal -t "GUI" --tab -- bash -c 'roslaunch robot_control gui.launch'
-sleep 1
+# sleep 1
